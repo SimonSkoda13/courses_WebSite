4a02e26a6293e680cba8b1bce09138e50165d577
SimonSkoda13 <80981006+SimonSkoda13@users.noreply.github.com>
2021-06-02 12:35:00 +0200

    Add files via upload

--- courses_django/courses_django/urls.py
+++ courses_django/courses_django/urls.py
@@ -1,11 +1,7 @@
-from django.conf import settings
-from django.conf.urls.static import static
-from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
+from product import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/v1/', include('djoser.urls')),
-    path('api/v1/', include('djoser.urls.authtoken')),
-    path('api/v1/', include('product.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('latest-products/', views.LatestProductsList.as_view()),
+    path('products/<slug:category_slug>/<slug:product_slug>/', views.ProductDetail.as_view()),
+]
